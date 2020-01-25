9160e118fd6145313be1793503c096beed70a4a6
Katie Durga <katie.durga@gmail.com>
2020-01-25 14:00:50 -0500

    everything working

--- liri.js
+++ liri.js
@@ -1,75 +1,112 @@
-// const moment = require("moment");
+const moment = require("moment");
 const fs = require('fs');
 require("dotenv").config();
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 const axios = require("axios");
-let currentDay = "2020-01-25";
-let oneWeek = "2020-02-01";
+
+
 //BANDS IN TOWN
-const searchConcerts = (query)=>{
-  let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp&date=${currentDay}%2C${oneWeek}`;
+const searchConcerts = str =>{
+  let url = `https://rest.bandsintown.com/artists/${str}/events?app_id=codingbootcamp`;
   axios.get(url).then((response)=>{
-      console.log(response.data[0]);
+    console.log(response.data[0].artist.name);
+    console.log(`Playing at ${response.data[0].venue.name}`)
+    console.log(`in ${response.data[0].venue.city}`)
+    console.log(`on ${moment(response.data[0].datetime).format("L")}`);
   }).catch(error =>{console.error(error)});
 }
+
 //SPOTIFY
-const searchSpotify = (str) => {
+const searchSpotify = str => {
   spotify.search({ type: 'track', query: str }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-  let artist = (data.tracks.items[0].artists[0].name);
-  let songName = data.tracks.items[0].name;
-  let album = data.tracks.items[0].album.name;
-  let songSample = (data.tracks.items[0].external_urls.spotify);
+  let artist = `Artist: ${data.tracks.items[0].artists[0].name}`;
+  let songName = `Song: ${data.tracks.items[0].name}`;
+  let album = `Album: ${data.tracks.items[0].album.name}`;
+  let songSample = `Link to Song: ${data.tracks.items[0].external_urls.spotify}`;
   console.log(songName);
   console.log(album);
   console.log(artist);
   console.log(songSample);
   });
 };
+
 //OMDB
-const searchMovies = (query)=>{
-  let url = `http://www.omdbapi.com/?apikey=[trilogy]&`;
+const searchMovies = str =>{
+  let url = `http://www.omdbapi.com/?apikey=trilogy&t=${str}`;
   axios.get(url).then((response)=>{
-      console.log(response.request);
+    console.log(`Title: ${response.data.Title}`); // Movie Title
+    console.log(`Year: ${response.data.Year}`); // Release Year
+    console.log(`IMDB Rating: ${response.data.imdbRating}`); // IMDB rating
+    console.log(`RT Rating: ${response.data.Ratings[1].Value}`); // Rotten Tomatoes Rating (N.B. IF ROTTEN TOMATOES DATA IS NOT RETURNED, THIS IS USUALLY A METACRITIC SCORE)
+    console.log(`Country: ${response.data.Country}`); // Production Country
+    console.log(`Language: ${response.data.Language}`); // Movie Language
+    console.log(`Actors: ${response.data.Actors}`); // Movie Actors
+    console.log(`Plot Summary: ${response.data.Plot}`); // Movie Plot
   }, (error)=>{
       console.log(error);
   });
 }
-if (process.argv.length > 2) {
-  try{
-    let command = process.argv[2].toLowerCase();
-    let media = ""
-    for(i=3; i<process.argv.length; i++){
-      media+= process.argv[i].toLowerCase()+" ";
+
+const rando = () => {
+  fs.readFile("random.text", "utf8", (error, data) => {
+    if (error) {
+      return console.log(error);
     }
+    let arr = data.split(",");
+    media = arr[1];
+    command = arr[0];
     switch (command) {
-      case 'concert':
+      case 'searchConcerts':
       {
         searchConcerts(media);
       }
       break;
-      case 'song':
+      case 'searchSpotify':
       {
         searchSpotify(media);
       }
       break;
-      case 'movie':
+      case 'searchMovies':
       {
-        console.log('film');
-      }
-      break;
-      case 'random':
-      {
-        console.log('hmm');
+        searchMovies(media);
       }
       break;
     };
-  }catch(error){
-    console.log(error);
-    console.log("Invalid entry, required input: 'command-query'");
-  }
-}else{console.log("Invalid Entry")};
+  })
+};
+
+try{
+  let command = process.argv[2].toLowerCase();
+  let media = process.argv[3]
+  
+  switch (command) {
+    case 'concert':
+    {
+      searchConcerts(media);
+    }
+    break;
+    case 'song':
+    {
+      searchSpotify(media);
+    }
+    break;
+    case 'movie':
+    {
+      searchMovies(media);
+    }
+    break;
+    case 'random':
+    {
+      rando();
+    }
+    break;
+  };
+}catch(error){
+  console.log(error);
+  console.log("Invalid entry, required input: 'command-query'");
+}
