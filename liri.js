c4108b7068605eae98341390db78f77f1a5063b8
Katie Durga <katie.durga@gmail.com>
2020-01-25 12:58:29 -0500

    Revert "axios error"
    This reverts commit 20b8e614c0a989b5d6151e9156912ee7fb55d810.

--- liri.js
+++ liri.js
@@ -1,23 +1,19 @@
 // const moment = require("moment");
+const axios = require("axios");
 const fs = require('fs');
 require("dotenv").config();
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
-const axios = require("axios");
 
-let currentDay = "2020-01-25";
-let oneWeek = "2020-02-01";
+console.log(spotify);
 
-//BANDS IN TOWN
-const searchConcerts = (query)=>{
-  let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp&date=${currentDay}%2C${oneWeek}`;
-  axios.get(url).then((response)=>{
-      console.log(response.data[0]);
-  }).catch(error =>{console.error(error)});
-}
 
-//SPOTIFY
+
+if (process.argv.length > 2) {
+
+
+
 const searchSpotify = (str) => {
   spotify.search({ type: 'track', query: str }, function(err, data) {
     if (err) {
@@ -31,54 +27,66 @@ const searchSpotify = (str) => {
   console.log(album);
   console.log(artist);
   console.log(songSample);
+  // console.log(data.tracks.items[0]); 
   });
 };
 
+// searchSpotify(media);
+// axios
+//   .get(``)
+//   .then(function(res) {
+//     console.log(res.data);
+//   });
 
-//OMDB
-const searchMovies = (query)=>{
-  let url = `http://www.omdbapi.com/?apikey=[trilogy]&`;
-  axios.get(url).then((response)=>{
-      console.log(response.request);
-  }, (error)=>{
-      console.log(error);
+function searchConcert(media) {
+  axios
+  .get(
+    `https://rest.bandsintown.com/artists/${media}/events?app_id=codingbootcamp`
+  )
+  .then(function(response) {
+    console.log(response); // Venue Name
+    // console.log(response.data[0].venue.city); // Venue City
+  })
+  .catch(function(error) {
+    console.log(error);
   });
 }
 
-
-
-if (process.argv.length > 2) {
-  try{
-    let command = process.argv[2].toLowerCase();
-    let media = ""
-    for(i=3; i<process.argv.length; i++){
-      media+= process.argv[i].toLowerCase()+" ";
-    }
-    switch (command) {
-      case 'concert':
-      {
-        searchConcerts(media);
-      }
-      break;
-      case 'song':
-      {
-        searchSpotify(media);
-      }
-      break;
-      case 'movie':
-      {
-        console.log('film');
-      }
-      break;
-      case 'random':
-      {
-        console.log('hmm');
-      }
-      break;
-    };
-
-  }catch(error){
-    console.log(error);
-    console.log("Invalid entry, required input: 'command-query'");
+try{
+  let command = process.argv[2].toLowerCase();
+  let media = ""
+  for(i=3; i<process.argv.length; i++){
+  media+= process.argv[i].toLowerCase()+" ";
   }
+// for (i=3; i<process.argv.length; i++) {
+//   media += process.argv[i];
+//   console.log(media);
+// };
+switch (command) {
+  case 'concert':
+  {
+    searchConcert(media);
+  }
+  break;
+  case 'song':
+  {
+    searchSpotify(media);
+  }
+  break;
+  case 'movie':
+  {
+    console.log('film');
+  }
+  break;
+  case 'random':
+  {
+    console.log('hmm');
+  }
+  break;
+};
+
+}catch(error){
+console.log(error);
+console.log("Invalid entry, required input: 'command-query'");
+}
 }else{console.log("Invalid Entry")};
